1a977b408bf68086e19bf9eab25953f35d35ff6a
Vishnu S <vs12339@gmail.com>
2021-10-14 23:32:22 +0530

    create products added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React,{useEffect,useContext} from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Signup from "./Pages/Signup";
 import Login from './Pages/Login'
+import Create from "./Pages/Create";
 import './App.css';
 import { AuthContext, FirebaseContext } from "./store/Context";
 
@@ -32,6 +33,9 @@ function App() {
         </Route>
         <Route path='/login'>
       <Login />
+        </Route>
+        <Route path='/create'>
+      <Create />
         </Route>
       </Router>
 
